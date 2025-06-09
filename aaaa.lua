808cc1fd417d2abd8baa41b1a2f2f56b4452a1cc
no <mys4exz4@gmail.com>
2025-06-09 13:41:19 +0700

    Update aaaa.lua

--- aaaa.lua
+++ aaaa.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7d547923627ff4097fb605ff69e20df1.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/7d547923627ff4097fb605ff69e20df1.lua"))()
